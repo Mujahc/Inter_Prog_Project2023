9a68b6d89d20f0a83720c7f7d236af1767d89bdb
Johann Culla-ag 2147185 <89601928+Mujahc@users.noreply.github.com>
2023-12-02 04:45:08 -0500

    .

--- Gym_Project/Gym.js
+++ Gym_Project/Gym.js
@@ -1,3 +1,4 @@
+// Gym.js
 document.addEventListener('DOMContentLoaded', function () {
     // Select buttons with class "signUpButton" and attach click event
     var signUpButtons = document.querySelectorAll('.signUpButton');
@@ -24,14 +25,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     var joinNowButton = document.querySelector('.formButton');
     joinNowButton.addEventListener('click', function () {
-        window.location.href = 'index.html';
-    });
-
-    // Get the login form and attach a submit event listener
-    var loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        simulateSuccessfulLogin();
+        setTimeout(function () {
+            window.location.href = 'index.html';
+        }, 2000);
     });
 });
 
@@ -43,10 +39,6 @@ function openYouTubeVideo(videoId) {
 }
 
 // Custom Dialog
-function redirectToHomePage() {
-    window.location.href = "Home.html";
-}
-
 function showSuccessDialog(message) {
     const dialog = document.createElement('div');
     dialog.classList.add('success-dialog');
@@ -55,34 +47,36 @@ function showSuccessDialog(message) {
 
     setTimeout(() => {
         dialog.remove();
-    }, 3000); // Remove the dialog after 3 seconds (adjust as needed)
+    }, 2000); // Remove the dialog after 3 seconds (adjust as needed)
 }
 
+// Simulate a successful signup
 document.addEventListener('DOMContentLoaded', function () {
     const signUpForm = document.getElementById('signupForm');
 
     signUpForm.addEventListener('submit', function (e) {
         e.preventDefault();
         const newUsername = document.getElementById('newUsername').value;
-        // Simulate a successful signup
+        
         showSuccessDialog(`Successful Sign Up. Welcome, ${newUsername}!`);
     });
 });
 
+// Simulate a successful login
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        
+        showSuccessDialog(`Successful Log In. Have a Good Workout, ${username}!`);
+    });
+});
 // function simulateSuccessfulLogin() {
 //     const username = document.getElementById('username').value;
 //     alert(`Successful Log In. Have a Good Workout, ${username}!`);
 // }
-document.addEventListener('DOMContentLoaded', function () {
-    const signUpForm = document.getElementById('loginForm');
-
-    signUpForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const newUsername = document.getElementById('username').value;
-        // Simulate a successful signup
-        showSuccessDialog(`Successful Log In. Have a Good Workout, ${username}!`);
-    });
-});
 
 // Deals
 document.addEventListener("DOMContentLoaded", function () {
